9278cc558823934b186089165e53d7f1e01c8b87
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-06-14 00:14:58 +0900

    Download file from DB

--- edit_ui/accessFIREBASE.py
+++ edit_ui/accessFIREBASE.py
@@ -1,16 +1,19 @@
-import pyrebase
 from firebase import Firebase
-
 import firebase_admin
 from firebase_admin import storage
 from firebase_admin import credentials
 
-cred = credentials.Certificate("C:/Users/이슬이/Documents/intelligentuiassistant-iua-firebase-adminsdk-in0kt-f3a89fbb76.json")
+cred = credentials.Certificate(
+    "C:/Users/이슬이/Documents/intelligentuiassistant-iua-firebase-adminsdk-in0kt-f3a89fbb76.json")
 firebase_admin.initialize_app(cred, {
-  'databaseURL': 'https://intelligentuiassistant-iua.firebaseio.com',
-  'storageBucket': 'intelligentuiassistant-iua.appspot.com',
+    'databaseURL': 'https://intelligentuiassistant-iua.firebaseio.com',
+    'storageBucket': 'intelligentuiassistant-iua.appspot.com',
 })
-file = 'edit_result/edit.png'
-blob = storage.bucket('intelligentuiassistant-iua.appspot.com').blob(file)
-blob.upload_from_filename(file)
+
+bucket_name = 'intelligentuiassistant-iua.appspot.com'
+source_blob_name = 'images/117뀨꺄.png'
+destination_file_name = 'edit_test/new.png'
+blob = storage.bucket(bucket_name).blob(source_blob_name)
+blob.download_to_filename(destination_file_name)
 print(blob.public_url) # prints https://storage.googleapis.com/my-app-name.appspot.com/a.jpg
+
