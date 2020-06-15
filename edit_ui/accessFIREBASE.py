ae9d3c06b802d1576e326f5ca328f9a7f0993ad3
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-06-16 02:43:28 +0900

    TimeStamp

--- edit_ui/accessFIREBASE.py
+++ edit_ui/accessFIREBASE.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import storage
 from firebase_admin import credentials
+from google.cloud import storage as st
 
 cred = credentials.Certificate(
     "C:/Users/이슬이/Documents/intelligentuiassistant-iua-firebase-adminsdk-in0kt-f3a89fbb76.json")
@@ -10,8 +11,23 @@ firebase_admin.initialize_app(cred, {
 })
 
 bucket_name = 'intelligentuiassistant-iua.appspot.com'
-source_blob_name = 'images/117뀨꺄.png'
-destination_file_name = 'edit_test/new.png'
+
+storage_client = st.Client()
+
+#Firebase storage 내에 있는 객체 목록 가져오기
+blobs = storage_client.list_blobs(bucket_name)
+img_list = []
+
+for b in blobs:
+    if b.name.split("/")[0] == 'images':
+        img_list.append(b.name.split("/")[1])
+        
+#타임스탬프가 가장 최근에 찍힌 파일 찾기
+img_list = sorted(img_list, reverse=True)
+source_blob_name = 'images/'+img_list[0]
+destination_file_name = 'edit_test/'+img_list[0]
+print(source_blob_name)
+#유저가 가장 최근에 올린 파일 서버에 저장
 blob = storage.bucket(bucket_name).blob(source_blob_name)
 blob.download_to_filename(destination_file_name)
 print(blob.public_url)
