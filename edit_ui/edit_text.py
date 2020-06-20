e637de49f287554f66bbba8ffcb6ab7a7687e649
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-06-21 00:17:00 +0900

    Change logic edit_text

--- edit_ui/edit_text.py
+++ edit_ui/edit_text.py
@@ -1,72 +1,36 @@
 import cv2
+import pytesseract
+from pytesseract import Output
 import numpy as np
 
-global clone, flag
-clone = None
-# 글자 설정
-color = (255, 0, 255)
-text = 'Check text!'
-font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-fontScale = 1.5
-thickness = 2
-
-
-def MouseLeftClick(event, x, y, flags, param):
-    # 왼쪽 마우스가 클릭되면 (x, y) 좌표를 저장한다.
-    if event == cv2.EVENT_LBUTTONDOWN:
-        # 원본 파일을 가져 와서 clicked_points에 있는 점들을 그린다.
-        image = clone.copy()
-        flag = True
-        px = x
-        py = y
-        cv2.putText(image, text, (x, y), font, fontScale, color, thickness)
-        cv2.imshow("image", image)
-        cv2.imwrite('edit_result/edit.png', image)
-
-    # 수정하고 싶은 이미지 경로
-
 def main(img):
-    image = cv2.imread('input_text/'+img)
-    large = cv2.resize(image, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    rgb = cv2.pyrDown(large)
-    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
-    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-    # using RETR_EXTERNAL instead of RETR_CCOMP
-    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    mask = np.zeros(bw.shape, dtype=np.uint8)
+    # kernel for sharpening
+    kernel_sharpen = np.array(
+        [[-1, -1, -1, -1, -1], [-1, 2, 2, 2, -1], [-1, 2, 8, 2, -1], [-1, 2, 2, 2, -1], [-1, -1, -1, -1, -1]]) / 8.0
+    # 정규화위해 8로나눔
 
-    for idx in range(len(contours)):
-        x, y, w, h = cv2.boundingRect(contours[idx])
-        mask[y:y + h, x:x + w] = 0
-        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
-        r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
-        if r > 0.45 and w > 8 and h > 8:
-            # 픽셀 값 추출
-            r, g, b = large[x, y]
+    origin = cv2.imread('input_text/'+img)
+
+    origin = cv2.resize(origin, None, fx=2.5, fy=2.5)
+    sharpen = cv2.filter2D(origin, -1, kernel_sharpen)
+
+    image = cv2.cvtColor(sharpen, cv2.COLOR_BGR2GRAY)
+    _, image = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
+
+    d = pytesseract.image_to_data(image, output_type=Output.DICT)
+
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 60:
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+
+            # print(x,y)
+            r, g, b = sharpen[y - 5, x - 5]
             r = int(r)
             g = int(g)
             b = int(b)
-            # 글자 영역 주변 색으로 칠하기
-            cv2.rectangle(rgb, (x, y), (x + w - 1, y + h - 1), (r, g, b), -1)
-    # show image with contours rect
-    # 문자 위치 선택 여부
-    flag = False
-    #cv2.namedWindow("image")
-    # cv2.setMouseCallback("image", MouseLeftClick)
-    cv2.imwrite('output_text/'+img, rgb)
+            image = cv2.rectangle(sharpen, (x - 5, y - 5), (x + w + 5, y + h + 5), (r, g, b), -1)
 
-    #clone = rgb.copy()
+    cv2.imwrite('output_text/'+img, image)
 
-    #while True:
-    #    cv2.imshow("image", rgb)
-    #    key = cv2.waitKey(0)
 
-    #    if key == ord('q'):  # 프로그램 종료
-    #        break
-
-    # 모든 window를 종료합니다.
-    #cv2.destroyAllWindows()
