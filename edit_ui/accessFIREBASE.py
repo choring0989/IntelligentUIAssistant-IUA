28a5b6fddcdb52e392a42a14ae030723c6d33cbc
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-06-20 16:28:05 +0900

    Main function completed!
    와아 기뻐용

--- edit_ui/accessFIREBASE.py
+++ edit_ui/accessFIREBASE.py
@@ -11,53 +11,75 @@ async def accept(websocket, path):
         # 클라이언트로부터 메시지를 대기한다.
         client_bla = await websocket.recv()
 
-        img_name = client_bla.split("&")[0]
-        model_name = client_bla.split("&")[1]
+        mode = client_bla.split("&")[0]
+        img_name = client_bla.split("&")[1]
+        model_name = client_bla.split("&")[2]
 
-        source_blob_name = 'images/' + img_name
-        destination_file_name = 'input/' + img_name
-        # 유저가 가장 최근에 올린 파일을 서버에 저장
-        blob = storage.bucket(bucket_name).blob(source_blob_name)
-        blob.download_to_filename(destination_file_name)
-        print(blob.public_url)
+        if mode == "SP":
+            source_blob_name = 'images/' + img_name
+            destination_file_name = 'input/' + img_name
+            # 유저가 가장 최근에 올린 파일을 서버에 저장
+            blob = storage.bucket(bucket_name).blob(source_blob_name)
+            blob.download_to_filename(destination_file_name)
+            print(blob.public_url)
 
-        zipDIR = 'C:/Users/이슬이/Documents/GitHub/IntelligentUIAssistant-IUA/edit_ui/output/'+img_name.split(".")[0]
+            zipDIR = 'C:/Users/이슬이/Documents/GitHub/IntelligentUIAssistant-IUA/edit_ui/output/'+img_name.split(".")[0]
 
-        # zip 파일을 저장할 디렉토리 생성
-        try:
-            if not os.path.exists(zipDIR):
-                os.makedirs(zipDIR)
-        except OSError:
-            print("dir error")
+            # zip 파일을 저장할 디렉토리 생성
+            try:
+                if not os.path.exists(zipDIR):
+                    os.makedirs(zipDIR)
+            except OSError:
+                print("dir error")
 
-        import seperate_ui
-        seperate_ui.main("model/"+model_name, img_name)
+            import seperate_ui
+            seperate_ui.main("model/"+model_name, img_name)
 
-        import zipfile
-        with zipfile.ZipFile(zipDIR+'/output.zip', 'w') as myZip:
-            # 압축 대상 경로
-            des_folder = zipDIR
-            # os 모듈로 해당 경로의 파일을 취득한다.(os.walk는 경로의 하위 폴더까지 취득한다.)
-            for folder, subfolders, files in os.walk(des_folder):
-            # os.walk로 파일 리스트를 받아 루프를 돌린다.
-                for file in files:
-                # folder와 files명은 다른 변수로 취득된다. 두 변수값으로 파일 절대 경로를 만든다.
-                    full_name = os.path.join(folder, file);
-                    # write은 압축 파일에 파일을 쓰는 것이다. 파라미터는 대상 파일, 저장될 경로로 지정된다.
-                    # 압축 파일에 저장될 경로는 \기준으로 상대 경로로 작성해야 한다.
-                    myZip.write(full_name, os.path.relpath(full_name, des_folder));
+            import zipfile
+            with zipfile.ZipFile(zipDIR+'/output.zip', 'w') as myZip:
+                # 압축 대상 경로
+                des_folder = zipDIR
+                # os 모듈로 해당 경로의 파일을 취득한다.(os.walk는 경로의 하위 폴더까지 취득한다.)
+                for folder, subfolders, files in os.walk(des_folder):
+                # os.walk로 파일 리스트를 받아 루프를 돌린다.
+                    for file in files:
+                    # folder와 files명은 다른 변수로 취득된다. 두 변수값으로 파일 절대 경로를 만든다.
+                        full_name = os.path.join(folder, file);
+                        # write은 압축 파일에 파일을 쓰는 것이다. 파라미터는 대상 파일, 저장될 경로로 지정된다.
+                        # 압축 파일에 저장될 경로는 \기준으로 상대 경로로 작성해야 한다.
+                        myZip.write(full_name, os.path.relpath(full_name, des_folder));
 
-        # 결과물 서버로 재전송
-        upload_file_name = 'output/' + img_name
-        blob = storage.bucket(bucket_name).blob(upload_file_name)
-        blob.upload_from_filename('output/' + img_name)
+            # 결과물 서버로 재전송
+            upload_file_name = 'output/' + img_name
+            blob = storage.bucket(bucket_name).blob(upload_file_name)
+            blob.upload_from_filename('output/' + img_name)
 
-        upload_file_name = 'zip/' + img_name.split(".")[0] + ".zip"
-        blob = storage.bucket(bucket_name).blob(upload_file_name)
-        blob.upload_from_filename(zipDIR+'/output.zip')
+            upload_file_name = 'zip/' + img_name.split(".")[0] + ".zip"
+            blob = storage.bucket(bucket_name).blob(upload_file_name)
+            blob.upload_from_filename(zipDIR+'/output.zip')
 
-        # 클라이언트에 처리 완료 알림
-        await websocket.send("success");
+            # 클라이언트에 처리 완료 알림
+            await websocket.send("success");
+
+        elif mode == "TD":
+            source_blob_name = 'images_text/' + img_name
+            destination_file_name = 'input_text/' + img_name
+            # 유저가 가장 최근에 올린 파일을 서버에 저장
+            blob = storage.bucket(bucket_name).blob(source_blob_name)
+            blob.download_to_filename(destination_file_name)
+            print(blob.public_url)
+
+            import edit_text
+            edit_text.main(img_name)
+
+            # 결과물 서버로 재전송
+            upload_file_name = 'output_text/' + img_name
+            blob = storage.bucket(bucket_name).blob(upload_file_name)
+            blob.upload_from_filename('output_text/' + img_name)
+
+            # 클라이언트에 처리 완료 알림
+            await websocket.send("success");
+
 
 # 파이어베이스 인증
 cred = credentials.Certificate(
--- edit_ui/edit_text.py
+++ edit_ui/edit_text.py
@@ -25,48 +25,48 @@ def MouseLeftClick(event, x, y, flags, param):
 
     # 수정하고 싶은 이미지 경로
 
+def main(img):
+    image = cv2.imread('input_text/'+img)
+    large = cv2.resize(image, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    rgb = cv2.pyrDown(large)
+    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+    # using RETR_EXTERNAL instead of RETR_CCOMP
+    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    mask = np.zeros(bw.shape, dtype=np.uint8)
 
-image = cv2.imread('edit_test/05.png')
-large = cv2.resize(image, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-rgb = cv2.pyrDown(large)
-small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
-_, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-# using RETR_EXTERNAL instead of RETR_CCOMP
-contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-mask = np.zeros(bw.shape, dtype=np.uint8)
+    for idx in range(len(contours)):
+        x, y, w, h = cv2.boundingRect(contours[idx])
+        mask[y:y + h, x:x + w] = 0
+        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+        r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
+        if r > 0.45 and w > 8 and h > 8:
+            # 픽셀 값 추출
+            r, g, b = large[x, y]
+            r = int(r)
+            g = int(g)
+            b = int(b)
+            # 글자 영역 주변 색으로 칠하기
+            cv2.rectangle(rgb, (x, y), (x + w - 1, y + h - 1), (r, g, b), -1)
+    # show image with contours rect
+    # 문자 위치 선택 여부
+    flag = False
+    #cv2.namedWindow("image")
+    # cv2.setMouseCallback("image", MouseLeftClick)
+    cv2.imwrite('output_text/'+img, rgb)
 
-for idx in range(len(contours)):
-    x, y, w, h = cv2.boundingRect(contours[idx])
-    mask[y:y + h, x:x + w] = 0
-    cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
-    r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
-    if r > 0.45 and w > 8 and h > 8:
-        # 픽셀 값 추출
-        r, g, b = large[x, y]
-        r = int(r)
-        g = int(g)
-        b = int(b)
-        # 글자 영역 주변 색으로 칠하기
-        cv2.rectangle(rgb, (x, y), (x + w - 1, y + h - 1), (r, g, b), -1)
-# show image with contours rect
-# 문자 위치 선택 여부
-flag = False
-cv2.namedWindow("image")
-# cv2.setMouseCallback("image", MouseLeftClick)
-cv2.imwrite('edit_result/edit.png', rgb)
+    #clone = rgb.copy()
 
-clone = rgb.copy()
+    #while True:
+    #    cv2.imshow("image", rgb)
+    #    key = cv2.waitKey(0)
 
-#while True:
-#    cv2.imshow("image", rgb)
-#    key = cv2.waitKey(0)
+    #    if key == ord('q'):  # 프로그램 종료
+    #        break
 
-#    if key == ord('q'):  # 프로그램 종료
-#        break
-
-# 모든 window를 종료합니다.
-cv2.destroyAllWindows()
+    # 모든 window를 종료합니다.
+    #cv2.destroyAllWindows()
